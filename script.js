0574e86bd72e6a85f55d56bb86ee1a0aff54997a
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2025-02-06 18:51:24 -0300

    pequenos ajustes e comecei a mexer no main

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Sidebar animation
 const sidebarToggle = document.getElementById('sidebar-toggle');
 const sidebarMenu = document.getElementById('sidebar-menu');
 const overlay = document.getElementById('overlay');
@@ -10,4 +11,26 @@ sidebarToggle.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     sidebarMenu.classList.remove('open');
     overlay.classList.remove('active');
+});
+
+// carousel-dots 
+const carousel = document.getElementById("news-carousel");
+const dots = document.querySelectorAll(".dot");
+
+function goToSlide(index) {
+    const cardWidth = document.querySelector(".card").offsetWidth;
+    carousel.scrollTo({
+        left: index * (cardWidth + 15), // 15px de gap
+        behavior: "smooth"
+    });
+
+    dots.forEach(dot => dot.classList.remove("active"));
+    dots[index].classList.add("active");
+}
+
+dots.forEach(dot => {
+    dot.addEventListener("click", () => {
+        const index = parseInt(dot.dataset.index);
+        goToSlide(index);
+    });
 });
